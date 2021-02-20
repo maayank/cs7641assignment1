1d7115917ce8b4ac53b890b4ae0db8f79ff08df4
Maayan Keshet <maayan@maayank.com>
2021-02-20 11:30:25 +0200

    current

--- common.py
+++ common.py
@@ -5,16 +5,14 @@ from time import perf_counter
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-from sklearn.tree import DecisionTreeClassifier
-
 class StubLearner:
     '''
     Abstract class representing a learner
     '''
-    def fit(self, df):
+    def fit(self, X, y):
         pass
 
-    def predict(self, df):
+    def predict(self, X):
         return [i % 2 for i in range(len(df))]
 
 class Experiment:
@@ -26,34 +24,38 @@ class Experiment:
         self.training_df, self.test_df = train_test_split(df, train_size=.8, shuffle=True)
         self.learner = learner
 
+    @staticmethod
+    def _split(df):
+        return df.iloc[:, :-1], df.iloc[:, -1]
+
     def _eval(self, df):
-        test_features = df.iloc[:, :-1]
-        test_y = df.iloc[:, -1]
+        test_features, test_y = self._split(df)
         start = perf_counter()
         pred_y = self.learner.predict(test_features)
         took = perf_counter() - start
-        print(f'Predicting took {took} seconds.')
+ #       print(f'Predicting took {took} seconds.')
 
         return tuple(func(test_y, pred_y) for func in [accuracy_score, f1_score, precision_score, recall_score])
 
 
     def do(self):
         start = perf_counter()
-        self.learner.fit(self.training_df)
+        self.learner.fit(*self._split(self.training_df))
         took = perf_counter() - start
-        print(f'Fitting took {took} seconds.')
+#        print(f'Fitting took {took} seconds.')
 
 
-        print('Predicting on training_df')
-        print(self._eval(self.training_df))
-        print('----------------')
+#        print('Predicting on training_df')
+#        print(self._eval(self.training_df))
+#        print('----------------')
 
-        print('Predicting on test_df')
-        print(self._eval(self.test_df))
+#        print('Predicting on test_df')
+        res = self._eval(self.test_df)
+#        print(res)
+        return res
 
 if __name__=='__main__':
     ex = Experiment(load_cancer(), StubLearner())
     print(ex.training_df)
     print(ex.test_df)
     print(ex.do())
-
--- load_datasets.py
+++ load_datasets.py
@@ -2,20 +2,28 @@ import pandas as pd
 
 def load_cancer():
     names = ['id'] + ['fut' + str(i) for i in range(1, 10)] +  ['malignant']
-    df = pd.read_csv('data/cancer/breast-cancer-wisconsin.data', sep=',', names=names)
+    df = pd.read_csv('data/cancer/breast-cancer-wisconsin.data.cleaned', sep=',', names=names)
     df.drop(columns=['id'], inplace=True)
     df['malignant'] = df['malignant'] == 4
     return df
 
 def load_wine():
     df1 = pd.read_csv('data/wine/winequality-red.csv', sep=';')
-    df1['type'] = 1
+    df1.insert(0, 'type', 1)
     df2 = pd.read_csv('data/wine/winequality-white.csv', sep=';')
-    df2['type'] = 2
+    df2.insert(0, 'type', 2)
     df = df1.append(df2, ignore_index=True)
     df['quality'] = df['quality'] > 5
+    return df
+
+def load_mushroom():
+    df = pd.read_csv('data/mushroom/agaricus-lepiota.data')
+#    df.drop(columns=['id'], inplace=True)
+#    df['malignant'] = df['malignant'] == 4
+    df = df.astype("category")
     return df
 
 if __name__=='__main__':
     print(load_cancer())
     print(load_wine())
+#    print(load_mushroom())
